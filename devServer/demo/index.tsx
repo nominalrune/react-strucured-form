df5e96fad9c0d59e484c96578ae33b153a51360a
yoshinori yokoyama <nominalrune.yokoyama@gmail.com>
2024-02-24 08:54:58 +0900

    hmmm

--- devServer/demo/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
-console.log('qqqqqqqqqqqqqqqqqqqq')
-document.addEventListener("DOMContentLoaded", () => {
-	const root = createRoot(document.getElementById('root')!);
-	root.render(<App/>);
-});
